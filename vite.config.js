f1c2af399e0f03559fe407323f039b692c8b943c
DonkeyKong55 <109015988+DonkeyKong55@users.noreply.github.com>
2025-08-31 12:33:11 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { reactRouter } from "@react-router/dev/vite";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// This code helps find the correct path to your 'src' folder
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -13,7 +12,6 @@ export default defineConfig({
       prerender: false,
     }),
   ],
-  // This section manually tells Vite what the "@" shortcut means
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
