4ed2c09a8d9ca29374fd5fe7a5058d3827ada01e
DonkeyKong55 <dkdesigns565@gmail.com>
2025-09-08 10:00:49 +1000

    Update React Router config to point to src/app

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  ssr: false,    // disable server rendering
-  prerender: false, // disable prerender step
+  appDirectory: "src/app",
+  buildDirectory: "build",
+  ssr: false,
+  prerender: false,
 } satisfies Config;
